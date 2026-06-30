b0248a3b40bb6201c6af29e31dc08c934ae5c290
Juliette <211356692+jfran01@users.noreply.github.com>
2026-06-30 14:57:50 +0100

    Test that game ends when appropriate And test that game can switch between players
    -> Saved in wrong order

---
+++ spec/game_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../lib/game'
+
+describe Game do
+  let(:game) { described_class.new }
+  describe '#change_player' do
+    it 'changes the current player' do
+      expect{game.change_player}.to change{game.current_player}
+    end
+  end
+
+  describe '#play_game' do
+    before do
+      allow(game).to receive(:play_round)
+      allow(game).to receive(:check_for_winner)
+      allow(game).to receive(:check_for_draw)
+      allow(game.instance_variable_get(:@player_one)).to receive(:choose_symbol)
+      allow(game.instance_variable_get(:@player_two)).to receive(:choose_symbol)
+    end
+    it 'ends game if there is a winner' do
+      allow(game).to receive(:check_for_winner).and_return(false, true)
+      game.play_game
+      expect(game).to have_received(:play_round).twice
+    end
+    it 'ends game if there is a draw' do
+      allow(game).to receive(:check_for_draw).and_return(false, true)
+      game.play_game
+      expect(game).to have_received(:play_round).twice
+    end
+  end
+end
