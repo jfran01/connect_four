a07b852eaadcafc37861f21d12350f33fe738f9e
Juliette <211356692+jfran01@users.noreply.github.com>
2026-06-30 15:55:40 +0100

    Tests that player can successfully choose their symbol

---
+++ spec/player_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../lib/player'
+
+describe Player do
+  let(:player){ Player.new }
+  describe '#choose_symbol' do
+    before do
+      # allow(player).to receive(:puts)
+    end
+    context 'when chosen number applies to the given array' do
+      it 'assigns the corresponding symbol to the symbol instance variable' do
+        allow(player).to receive(:gets).and_return('2')
+        expect{player.choose_symbol}.to change{player.symbol}.to("\e[31m\u2b24\e[0m")
+      end
+    end
+    context 'when chosen number does not apply to the given array' do
+      it 'accepts other numbers as symbol' do
+        allow(player).to receive(:gets).and_return('24')
+        expect{player.choose_symbol}.to change{player.symbol}.to("24")
+      end
+      it 'accepts letters as symbol' do
+        allow(player).to receive(:gets).and_return('X')
+        expect{player.choose_symbol}.to change{player.symbol}.to("X")
+      end
+      it 'accepts other unicode symbols as symbol' do
+        allow(player).to receive(:gets).and_return("\u232c")
+        expect{player.choose_symbol}.to change{player.symbol}.to("\u232c")
+      end
+    end
+  end
+end
