bf51fb8b69c2588512804bec414a82529f0d1de5
Juliette <211356692+jfran01@users.noreply.github.com>
2026-07-01 09:15:01 +0100

    Check column chosen by player is in range and not full

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,11 @@ require_relative '../lib/player'
 
 describe Player do
   let(:player){ Player.new }
+  before do
+    allow(player).to receive(:puts)
+    allow(player).to receive(:gets)
+  end
   describe '#choose_symbol' do
-    before do
-      # allow(player).to receive(:puts)
-    end
     context 'when chosen number applies to the given array' do
       it 'assigns the corresponding symbol to the symbol instance variable' do
         allow(player).to receive(:gets).and_return('2')
@@ -24,6 +25,33 @@ describe Player do
       it 'accepts other unicode symbols as symbol' do
         allow(player).to receive(:gets).and_return("\u232c")
         expect{player.choose_symbol}.to change{player.symbol}.to("\u232c")
+      end
+    end
+  end
+
+  describe "#check_choice" do
+    before do
+      allow(player).to receive(:choose_column)
+    end
+    context 'when choice is in an in-range number and not full' do
+      let(:board) { Array.new(3, Array.new(3)) }
+      it 'assigns choice to column_choice' do
+        chosen_column = 1
+        expect(player.check_choice(chosen_column, board)).to eq(true)
+      end
+    end
+    context 'when choice is out of range' do
+      let(:board) { Array.new(3, Array.new(3)) }
+      it 'calls choose_column' do
+        chosen_column = 9
+        expect(player.check_choice(chosen_column, board)).to eq(false)
+      end
+    end
+    context 'when column is full' do
+      let(:board) {[[nil, nil, 1], [nil, nil, 1], [nil, nil, 1]]}
+      it 'calls choose_column' do
+        chosen_column = 2
+        expect(player.check_choice(chosen_column, board)).to eq(false)
       end
     end
   end
